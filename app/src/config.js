84a890c5109144e9654a47b0fb5fe3060ed3de07
gaku3601 <pro.gaku@gmail.com>
2018-05-31 08:29:01 +0900

    init up

---
+++ app/src/config.js
@@ -0,0 +1,6 @@
+export default {
+  Region: 'ap-northeast-1',
+  UserPoolId: 'ap-northeast-1_FcptEeIZt',
+  ClientId: '50sasn51dajdtsag8ei0nfp4bu', // appclientID
+  IdentityPoolId: 'arn:aws:cognito-idp:ap-northeast-1:982976011432:userpool/ap-northeast-1_FcptEeIZt'
+}
